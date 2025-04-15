d6a06c885d0f49e1d85d663860882da0e137c65c
AlloraPrego <jasmyusdt36@gmail.com>
2025-04-15 16:28:41 +0200

    Update config.js

--- config.js
+++ config.js
@@ -4,7 +4,7 @@ module.exports = {
   TOKEN: "",
   language: "en",
   ownerID: ["1004206704994566164", ""], 
-  mongodbUri : "",
+  mongodbUri : "mongodb+srv://2pacifia2:2pacifia2@discord0.qtvn6pf.mongodb.net/?retryWrites=true&w=majority",
   spotifyClientId : "",
   spotifyClientSecret : "",
   setupFilePath: './commands/setup.json',
